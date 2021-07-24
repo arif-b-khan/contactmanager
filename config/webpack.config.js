21e69f1f74f778c08290b8a950b578cc526209f3
Arif Khan <arif.bannehasan@gmail.com>
2021-07-24 12:40:00 +0530

    TASK: Adding debug support

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
-const root = path.dirname(__dirname)
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+const root = path.dirname(__dirname);
 
+console.log(`Port Number: ${process.env.PORT}`);
 module.exports = {
     entry: `${root}/app/server.ts`,
     target: 'node',
+    devtool: 'inline-source-map',
     externals: [
         /^[a-z\-0-9]+$/ // Ignore node_modules folder
     ],
     output: {
-        filename: 'compiled', // output file
+        filename: 'compiled.js', // output file
         path: `${root}/dist`,
         libraryTarget: "commonjs"
     },
@@ -34,8 +38,17 @@ module.exports = {
             use: [
                 {
                     loader: 'ts-loader',
-                }
+                },
+                "source-map-loader"
             ]
         }]
-    }
+    },
+    plugins: [  new Dotenv({
+        // path: './.env.debug', // load this now instead of the ones in '.env'
+        // safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file. 
+        allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
+        systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+        silent: true, // hide any errors
+        defaults: false // load '.env.defaults' as the default values if empty.
+      })]
 };
